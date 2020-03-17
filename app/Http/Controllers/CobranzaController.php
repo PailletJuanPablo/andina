eebe8e97e4ad615be7fead4ec43f49b4a6998a88
Juan Pablo Paillet <pailletjuanpablo@gmail.com>
2020-03-17 12:17:17 -0300

    Fx

--- app/Http/Controllers/CobranzaController.php
+++ app/Http/Controllers/CobranzaController.php
@@ -262,7 +262,7 @@ class CobranzaController extends AppBaseController
              $cobranzas = Cobranza::
         
          //    where('employee_id', Auth::user()->id)
-            ->whereBetween('operation_date', [$from, $to])
+            whereBetween('operation_date', [$from, $to])
             ->orderBy('operation_date', 'DESC')
             ->get();
         }
